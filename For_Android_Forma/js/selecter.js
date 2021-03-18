c2b0fbea7b1fcd8f649f0cca1e379d2ae9cb32fa
hakupy <hakubaicho@gmail.com>
2021-03-18 17:55:22 +0900

    Add files via upload

--- For_Android_Forma/js/selecter.js
+++ For_Android_Forma/js/selecter.js
@@ -1,107 +1,102 @@
 // 'use strict';
 
 {
-  // 画像パスを配列で保持
-  const images = [
+  // *********************************************
+  // クラス定義
+  // *********************************************
+  // 洋服クラス
+  class ClothItem {
+    //----------------------------------
+    // コンストラクタ
+    //----------------------------------
+    // design       : デザイン
+    // wearedImage  : 着衣イメージ
+    // tag1         : 大分類
+    // tag2         : 中分類
+    // tag3         : 小分類
+    constructor(design, wearedImage, tag1,tag2, tag3)
+    {
+      this.design = design;
+      this.wearedImage = wearedImage;
+      this.tag1 = tag1;
+      this.tag2 = tag2;
+      this.tag3 = tag3;
+    }
+    //----------------------------------
+    // メソッド
+    //----------------------------------
+  }
+  // *********************************************
+  // インスタンスの作成
+  // *********************************************
+  // ClothItemのインスタンスを生成。
+  const items = [
+    new ClothItem('img/0001.png','img/s0001.png','ねこ','どうぶつ',''),
+    new ClothItem('img/0002.png','img/s0002.png','ねこ','どうぶつ',''),
+    new ClothItem('img/0003.png','img/s0003.png','おばけ','',''),
+    new ClothItem('img/0004.png','img/s0004.png','ねこ','どうぶつ',''),
+    new ClothItem('img/0005.png','img/s0005.png','いぬ','どうぶつ',''),
+    new ClothItem('img/0006.png','img/s0006.png','とり','どうぶつ',''),
+    new ClothItem('img/0007.png','img/s0007.png','ねこ','どうぶつ',''),
+    new ClothItem('img/0008.png','img/s0008.png','ねずみ','どうぶつ',''),
+    new ClothItem('img/0009.png','img/s0009.png','おばけ','',''),
+    new ClothItem('img/0010.png','img/s0010.png','おばけ','',''),
 
-    'img/0001.png',
-    'img/0002.png',
-    'img/0003.png',
-    'img/0004.png',
-    'img/0005.png',
-    'img/0006.png',
-    'img/0007.png',
-    'img/0008.png',
-    'img/0009.png',
-    'img/0010.png',
+    new ClothItem('img/0011.png','img/s0011.png','たいよう','',''),
+    new ClothItem('img/0012.png','img/s0012.png','くつ','',''),
+    new ClothItem('img/0013.png','img/s0013.png','くま','どうぶつ',''),
+    new ClothItem('img/0014.png','img/s0014.png','いぬ','どうぶつ',''),
+    new ClothItem('img/0015.png','img/s0015.png','おんなのこ','ひと',''),
+    new ClothItem('img/0016.png','img/s0016.png','おんなのこ','ひと',''),
+    new ClothItem('img/0017.png','img/s0017.png','うま','どうぶつ',''),
+    new ClothItem('img/0018.png','img/s0018.png','きりん','どうぶつ',''),
+    new ClothItem('img/0019.png','img/s0019.png','さる','どうぶつ',''),
+    new ClothItem('img/0020.png','img/s0020.png','ぶた','どうぶつ',''),
+    
+    new ClothItem('img/0021.png','img/s0021.png','かば','どうぶつ',''),
+    new ClothItem('img/0022.png','img/s0022.png','うさぎ','どうぶつ',''),
+    new ClothItem('img/0023.png','img/s0023.png','おとこのこ','ひと',''),
+    new ClothItem('img/0024.png','img/s0024.png','こども','ひと',''),
+    new ClothItem('img/0025.png','img/s0025.png','おとこのこ','ひと',''),
+    new ClothItem('img/0026.png','img/s0026.png','おとこのこ','ひと',''),
+    new ClothItem('img/0027.png','img/s0027.png','おんなのこ','ひと',''),
+    new ClothItem('img/0028.png','img/s0028.png','うさぎ','どうぶつ',''),
+    new ClothItem('img/0029.png','img/s0029.png','いぬ','どうぶつ',''),
+    new ClothItem('img/0030.png','img/s0030.png','ねこ','どうぶつ',''),
 
-    'img/0011.png',
-    'img/0012.png',
-    'img/0013.png',
-    'img/0014.png',
-    'img/0015.png',
-    'img/0016.png',
-    'img/0017.png',
-    'img/0018.png',
-    'img/0019.png',
-    'img/0020.png',
+    new ClothItem('img/0031.png','img/s0031.png','ぞう','どうぶつ',''),
+  ];
 
-    'img/0021.png',
-    'img/0022.png',
-    'img/0023.png',
-    'img/0024.png',
-    'img/0025.png',
-    'img/0026.png',
-    'img/0027.png',
-    'img/0028.png',
-    'img/0029.png',
-    'img/0030.png',
-
-    'img/0031.png',
-  ]
-  const imagesTryOn = [
-
-    'img/s0001.png',
-    'img/s0002.png',
-    'img/s0003.png',
-    'img/s0004.png',
-    'img/s0005.png',
-    'img/s0006.png',
-    'img/s0007.png',
-    'img/s0008.png',
-    'img/s0009.png',
-    'img/s0010.png',
-
-    'img/s0011.png',
-    'img/s0012.png',
-    'img/s0013.png',
-    'img/s0014.png',
-    'img/s0015.png',
-    'img/s0016.png',
-    'img/s0017.png',
-    'img/s0018.png',
-    'img/s0019.png',
-    'img/s0020.png',
-
-    'img/s0021.png',
-    'img/s0022.png',
-    'img/s0023.png',
-    'img/s0024.png',
-    'img/s0025.png',
-    'img/s0026.png',
-    'img/s0027.png',
-    'img/s0028.png',
-    'img/s0029.png',
-    'img/s0030.png',
-
-    'img/s0031.png',
-  ]
-  let items = new Array();
   // 現在選択中の画像
-  let currentIndex = 0;
+  let currentIndex = -1;
 
-  // メイン画像の設定
-  const mainImage = document.getElementById('thumbnail_selected');
-  const mainImageTryOn = document.getElementById('thumbnail_selected_tryon');
-  mainImage.src = images[currentIndex];
-  mainImageTryOn.src = imagesTryOn[currentIndex];
+  // タグ要素の取得メイン画像の設定
+  const imgDesign = document.getElementById('thumbnail_selected');
+  const imgWeared = document.getElementById('thumbnail_selected_tryon');
+  // imgDesign.src = items[currentIndex].design;
+  // imgWeared.src = items[currentIndex].wearedImage;
   
+  //--------------------------------------------
   // アイテムリストを生成します。
+  //--------------------------------------------
   function setItemList() {
+
+    const  targetTag = document.querySelector('.thumbnails');
+    console.log('setItemList');
+    while( targetTag.firstChild ){
+      targetTag.removeChild( targetTag.firstChild );
+    }
     //------------------------------------
     // thumbnailsの処理
     //------------------------------------
-    images.forEach((image, index) => {
-      items.push(image);
-      const img = document.createElement('img');
-  
+    items.forEach((item, index) => {
       //----------------------------------
       // 画像の表示
       //----------------------------------
-      // 画像ファイルで表示
-      img.src = image;
-      // 画像をBase64Blobで管理
-      // img.src = get_ImageBlob_fromIndex(index);
+      // imgタグを生成し、イメージを割り当て
+      const img = document.createElement('img');
+      img.src = item.design;
+      // li要素の生成
       const li = document.createElement('li');
   
       // 現在の選択中
@@ -109,25 +104,126 @@
       {
         li.classList.add('current');
       }
+
       // クリックされた時の処理
       li.addEventListener('click',() => {
         // 画像の切り替え
-        mainImage.src = image;
-        mainImageTryOn.src = imagesTryOn[index];
+        imgDesign.src = item.design;
+        imgWeared.src = item.wearedImage;
+
         // thumbnailsのすべての要素を取得
         const thumbnails = document.querySelectorAll('.thumbnails > li');
         // 現在の付加されているcurrentを外す
-        thumbnails[currentIndex].classList.remove('current');
+        if(0 <= currentIndex)
+        {
+          thumbnails[currentIndex].classList.remove('current');
+        }
+        
         // currentの更新
         currentIndex = index;
         thumbnails[currentIndex].classList.add('current');
-        console.log('');
+
       });
   
       li.appendChild(img);
-      document.querySelector('.thumbnails').appendChild(li);
+      targetTag.appendChild(li);
     });
   }
+  function setItemListTag(select_tag) {
+    console.log('setItemListTag');
+    // アイテム要素をクリア
+    const  targetTag = document.querySelector('.thumbnails');
+    while( targetTag.firstChild ){
+      targetTag.removeChild( targetTag.firstChild );
+    }
+
+    let keyWord = '';
+    if('string' == typeof(select_tag))
+    {
+      keyWord = select_tag;
+    }
+    else
+    {
+      // https://qiita.com/rukiadia/items/03aaa8955c0f6576a5e7
+      // addEventHandlerで定義した場合
+      if('target' in select_tag) {
+        target = select_tag.target;
+        if('eventParam' in target){
+          keyWord = select_tag.target.eventParam;
+        }
+      }
+    }
+    //------------------------------------
+    // thumbnailsの処理
+    //------------------------------------
+    items.forEach((item, index) => {
+      // タグが一致するか確認
+      let isHit = false;
+      // アイテムのタグエリアを取得する
+      const item_tags =[item.tag1, item.tag2, item.tag3];
+      // 配列に含まれていなければ追加
+      item_tags.forEach((item_tag) => {
+        if(item_tag != '')
+        {
+          if(keyWord == item_tag)
+          {
+            isHit = true;
+          }
+          else
+          {
+            if(keyWord == '')
+            {
+              isHit = true;
+            }
+          }
+        }  
+      });
+      // 不一致で処理中止
+      if(!isHit) {
+        return;
+      }
+      //----------------------------------
+      // 画像の表示
+      //----------------------------------
+      // imgタグを生成し、イメージを割り当て
+      const img = document.createElement('img');
+      img.src = item.design;
+      // li要素の生成
+      const li = document.createElement('li');
+  
+      // 現在の選択中
+      if(index === currentIndex)
+      {
+        li.classList.add('current');
+      }
+
+      // クリックされた時の処理
+      li.addEventListener('click',() => {
+        // 画像の切り替え
+        imgDesign.src = item.design;
+        imgWeared.src = item.wearedImage;
+
+        // thumbnailsのすべての要素を取得
+        const thumbnails = document.querySelectorAll('.thumbnails > li');
+        // 現在の付加されているcurrentを外す
+        if(0 <= currentIndex)
+        {
+          thumbnails[currentIndex].classList.remove('current');
+        }
+        
+        // currentの更新
+        currentIndex = index;
+        thumbnails[currentIndex].classList.add('current');
+
+      });
+  
+      li.appendChild(img);
+      targetTag.appendChild(li);
+    });
+  }
+  //--------------------------------------------
+  // カテゴリーリストを生成します。
+  //--------------------------------------------
   function setCategoryList() {
     const CATE_MAX = 20;
     const categorys = document.getElementById("categorys");
@@ -145,63 +241,61 @@
       categorys.appendChild(li);
     }
   }
+  //--------------------------------------------
+  // カテゴリーリストを生成します。
+  //--------------------------------------------
+  function setCategoryListByItemTag() {
+    const tags= [];
+    //-----
+    // アイテムごとにタグを取得する
+    //-----
+    items.forEach((item) => {
+
+      // アイテムのタグエリアを取得する
+      const item_tags =[item.tag1, item.tag2, item.tag3];
+      // 配列に含まれていなければ追加
+      item_tags.forEach((item_tag) => {
+        if(item_tag != '')
+        {
+          if(!tags.includes(item_tag))
+          {
+            tags.push(item_tag);
+          }
+        }  
+      });
+    });
+    // console.log(tags);
+
+    //-----
+    // カテゴリーボタンを生成
+    //-----
+    const categorys = document.getElementById("categorys");
+    while( categorys.firstChild ){
+      categorys.removeChild( categorys.firstChild );
+    }
+
+    // 要素を作る
+    tags.forEach((tag) => {
+      const li = document.createElement('li');
+      // ボタン要素を追加する
+      const button = document.createElement('button');
+      button.textContent = `${tag}`;
+      // イベントの追加
+      // ×）これやると即時実行になります。
+      // button.addEventListener("click", setItemList(tag), false);
+      // 〇） https://qiita.com/rukiadia/items/03aaa8955c0f6576a5e7
+      button.addEventListener("click", setItemListTag, false);
+      button.eventParam = tag;
+
+      li.appendChild(button);
+      // 配置
+      categorys.appendChild(li);
+    });
+  }
 
   // 画面構成を作る
   setItemList();
   setCategoryList();
-  // //------------------------------------
-  // // nextの処理
-  // //------------------------------------
-  // const next = document.getElementById('next');
-  // next.addEventListener( 'click',() => {
-  //   let target = currentIndex + 1;
-  //       // loop
-  //   if(target >= images.length)
-  //   {
-  //     target = 0;
-  //   }
-  //   const thumbnails = document.querySelectorAll('.thumbnails > li')[target].click();
-  // });
-  // //------------------------------------
-  // // prevの処理
-  // //------------------------------------
-  // const prev = document.getElementById('prev');
-  // prev.addEventListener('click', () => {
-  //   let target = currentIndex -1;
-  //   // loop
-  //   if(target < 0)
-  //   {
-  //     target = images.length - 1;
-  //   }
-  //   const thumnails = document.querySelectorAll('.thumbnails > li')[target].click();
-  // });
-  // //------------------------------------
-  // // playの処理
-  // //------------------------------------
-  // let isPlaying = false;
-  // let timeoutID;
-  // const play = document.getElementById('play');
-  // play.addEventListener('click', () => {
-  //   if(isPlaying === false)
-  //   {
-  //     playSlideShow();
-  //     play.textContent='Pause';
-  //   }
-  //   else
-  //   {
-  //     clearTimeout(timeoutID);
-  //     play.textContent='Play';
-  //   }
-  //   isPlaying = !isPlaying;
-  
-  // });
-
-  // function playSlideShow()
-  // {
-  //   // 一定時間ごとに処置
-  //   timeoutID = setTimeout(() =>{
-  //     next.click();
-  //     playSlideShow();
-  //   }, 1000);
-  // }
+  setCategoryListByItemTag();
+  setItemListTag('');  // これはだめ
 }
