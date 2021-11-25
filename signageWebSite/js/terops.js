e5b80f9351b0f39c879120f406799b71cfc98be2
hakupy <hakubaicho@gmail.com>
2021-11-25 14:13:45 +0900

    Add files via upload

---
+++ signageWebSite/js/terops.js
@@ -0,0 +1,40 @@
+let teropArray = [];
+let counterTerop = 0;
+let playingIDTerop = 0;
+
+/**
+ * テロップデータを更新する。
+ * @param {string[]} param 文字列配列 
+ */
+function reloadTerop(param){
+	teropArray=[];
+	for(let i=0;i<param.length;i++)
+	{
+		const terop = {terop:param[i].terop, title:""};
+		teropArray.push(terop);
+	}
+}
+/**
+ * 画僧の変更
+ */
+function changeTerop() {
+	if (counterTerop < teropArray.length) {
+		document.getElementById('terops').innerText = teropArray[counterTerop].terop;
+		counterTerop++;
+	} else {
+		document.getElementById('terops').innerText = teropArray[0].terop;
+		counterTerop = 1;
+	}
+}
+
+/**
+ * スライドショーの開始・停止
+ */
+function playTerop() {
+	if (playingIDTerop == 0) {
+		playingIDTerop = setInterval(changeTerop, 2000);
+	} else {
+		clearInterval(playingIDTerop);
+		playingIDTerop = 0;
+	}
+}
